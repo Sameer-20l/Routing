5ddbf75900db225c7f42392c8f121b786df79e5b
Sameer-20l <sameer.shende154@gmail.com>
2024-04-30 16:00:34 +0530

    initial comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule,AboutComponent,UserComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
